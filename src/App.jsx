ed987d309906b01002ec3b172bac0750631dfa28
Ihab <ihab.mortada1@gmail.com>
2023-06-05 15:07:52 +0200

    main routers

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import * as React from "react"
+import { createRoot } from "react-dom/client"
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom"
+import Dashboard, { dashboardLoader } from "./pages/Dashboard"
+import Error from "./pages/error"
+import Main, { mainLoader } from "./layouts/Main"
+import { logoutAction } from "./actions/logout"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    loader: mainLoader,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+        loader: dashboardLoader,
+        errorElement: <Error />,
+      },
+      {
+        path: "logout",
+        action: logoutAction,
+      },
+    ],
+  },
+])
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+      <ToastContainer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { fetchData } from "../helpers"
+import { useLoaderData } from "react-router-dom"
+
+export const dashboardLoader = () => {
+  const userName = fetchData("userName")
+  return { userName }
+}
+
+const Dashboard = () => {
+  const { userName } = useLoaderData()
+  return (
+    <div>
+      <h1>{userName}</h1>
+    </div>
+  )
+}
+
+export default Dashboard
